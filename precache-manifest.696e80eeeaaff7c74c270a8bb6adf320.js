1444c054e224d49aaddc4000e842424489fd77af
Pragati <priyadarshinip.1234@gmail.com>
2022-11-10 21:20:54 +0530

    Updates

---
+++ precache-manifest.696e80eeeaaff7c74c270a8bb6adf320.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "03cc5d7f0706ba19111b",
+    "url": "/react-movies-app/static/css/main.20f22be2.chunk.css"
+  },
+  {
+    "revision": "03cc5d7f0706ba19111b",
+    "url": "/react-movies-app/static/js/main.03cc5d7f.chunk.js"
+  },
+  {
+    "revision": "d2fd5e860431f1be0b24",
+    "url": "/react-movies-app/static/js/1.d2fd5e86.chunk.js"
+  },
+  {
+    "revision": "059829b07495b78e6beb",
+    "url": "/react-movies-app/static/js/2.059829b0.chunk.js"
+  },
+  {
+    "revision": "a9dcc37d3febda3cc77d",
+    "url": "/react-movies-app/static/js/runtime~main.a9dcc37d.js"
+  },
+  {
+    "revision": "87a9c64f6994703f116485674e473adc",
+    "url": "/react-movies-app/index.html"
+  }
+];
